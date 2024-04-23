c284b11abfe11ba4cda843621a0a364779b39687
danilovalerio <danilo_vs@hotmail.com>
2024-04-23 14:55:27 -0300

    variaveis de ambiente e get basico funcionando

--- src/main/java/com/danilo/learn/starwarsplanetapi/web/PlanetController.java
+++ src/main/java/com/danilo/learn/starwarsplanetapi/web/PlanetController.java
@@ -3,6 +3,7 @@ package com.danilo.learn.starwarsplanetapi.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,11 @@ public class PlanetController {
         Planet planetCreated = planetService.create(planet);
         return ResponseEntity.status(HttpStatus.CREATED).body(planetCreated);
 
+    }
+
+    @GetMapping
+    public ResponseEntity<String> get() {
+        return ResponseEntity.status(HttpStatus.CREATED).body("Olá");
     }
 
 }
