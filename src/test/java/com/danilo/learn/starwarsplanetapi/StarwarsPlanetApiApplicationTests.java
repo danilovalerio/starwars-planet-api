b9bc89c839d35d87fc22434708d16e35080763d0
danilovalerio <danilo_vs@hotmail.com>
2024-04-23 13:57:48 -0300

    first commit

---
+++ src/test/java/com/danilo/learn/starwarsplanetapi/StarwarsPlanetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.learn.starwarsplanetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsPlanetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
