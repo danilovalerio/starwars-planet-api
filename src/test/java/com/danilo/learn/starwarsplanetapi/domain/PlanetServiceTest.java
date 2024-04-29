be0e239372abaf82d626766f6b9b984d5a300413
danilovalerio <danilo_vs@hotmail.com>
2024-04-29 13:52:06 -0300

    AAA Princípio de Testes e o Primeiro teste com Mockito

--- src/test/java/com/danilo/learn/starwarsplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/com/danilo/learn/starwarsplanetapi/domain/PlanetServiceTest.java
@@ -1,9 +1,12 @@
 package com.danilo.learn.starwarsplanetapi.domain;
 
+import static org.mockito.Mockito.when;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.danilo.learn.starwarsplanetapi.common.PlanetConstants;
 
@@ -13,12 +16,21 @@ public class PlanetServiceTest {
     @Autowired
     private PlanetService planetService;
 
+    @MockBean
+    private PlanetRepository planetRepository;
+
     // operacao_estado_retorno
     @Test
     public void createPlanet_WithValidData_ReturnsPlanet() {
-        // system under test
+        // Princípio AAA dos Testes (Arrange Act Assert)
+        // Arrange (prepara os dados)
+        when(planetRepository.save(PlanetConstants.PLANET)).thenReturn(PlanetConstants.PLANET);
+
+        // system under test (sut)
+        // Act (agir, fazer a operação de fato)
         Planet sut = planetService.create(PlanetConstants.PLANET);
 
+        // Assert (aferir se o sistema sobre teste é o que esperada)
         Assertions.assertThat(sut).isEqualTo(PlanetConstants.PLANET);
 
     }
