c7b153aa387405ad5660ee5e9eaa2f9199f5ced1
danilovalerio <danilo_vs@hotmail.com>
2024-07-31 10:33:50 -0300

    Cenário de erro - definindo teste com base no design de sistema

--- src/test/java/com/danilo/learn/starwarsplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/com/danilo/learn/starwarsplanetapi/domain/PlanetServiceTest.java
@@ -1,5 +1,8 @@
 package com.danilo.learn.starwarsplanetapi.domain;
 
+import static com.danilo.learn.starwarsplanetapi.common.PlanetConstants.INVALID_PLANET;
+import static com.danilo.learn.starwarsplanetapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 import org.assertj.core.api.Assertions;
@@ -8,7 +11,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import com.danilo.learn.starwarsplanetapi.common.PlanetConstants;
 
 //@SpringBootTest(classes = PlanetService.class) // monta o contexto do spring apenas com a classe adicionada e deixa disponível para injecao
 @ExtendWith(MockitoExtension.class)
@@ -26,14 +28,22 @@ public class PlanetServiceTest {
     public void createPlanet_WithValidData_ReturnsPlanet() {
         // Princípio AAA dos Testes (Arrange Act Assert)
         // Arrange (prepara os dados)
-        when(planetRepository.save(PlanetConstants.PLANET)).thenReturn(PlanetConstants.PLANET);
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
 
         // system under test (sut)
         // Act (agir, fazer a operação de fato)
-        Planet sut = planetService.create(PlanetConstants.PLANET);
+        Planet sut = planetService.create(PLANET);
 
         // Assert (aferir se o sistema sobre teste é o que esperada)
-        Assertions.assertThat(sut).isEqualTo(PlanetConstants.PLANET);
+        Assertions.assertThat(sut).isEqualTo(PLANET);
+    }
+
+    // testando cenário de erro
+    @Test
+    public void createPlanet_WithInvalidData_ReturnsThrowsException(){
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
     }
 
 }
