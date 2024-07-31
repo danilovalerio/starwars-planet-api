c0d757c03d78b1ac1379289850e7a8273caaf581
danilovalerio <danilo_vs@hotmail.com>
2024-07-31 16:36:54 -0300

    FindById Planet quando existe e não existe o id

--- src/test/java/com/danilo/learn/starwarsplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/com/danilo/learn/starwarsplanetapi/domain/PlanetServiceTest.java
@@ -2,15 +2,17 @@ package com.danilo.learn.starwarsplanetapi.domain;
 
 import static com.danilo.learn.starwarsplanetapi.common.PlanetConstants.INVALID_PLANET;
 import static com.danilo.learn.starwarsplanetapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 //@SpringBootTest(classes = PlanetService.class) // monta o contexto do spring apenas com a classe adicionada e deixa disponível para injecao
 @ExtendWith(MockitoExtension.class)
@@ -35,7 +37,7 @@ public class PlanetServiceTest {
         Planet sut = planetService.create(PLANET);
 
         // Assert (aferir se o sistema sobre teste é o que esperada)
-        Assertions.assertThat(sut).isEqualTo(PLANET);
+        assertThat(sut).isEqualTo(PLANET);
     }
 
     // testando cenário de erro, quando dados invalidos e quando planeta existe
@@ -44,6 +46,25 @@ public class PlanetServiceTest {
         when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
 
         assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet() {
+       when(planetRepository.findById(1L)).thenReturn(java.util.Optional.of(PLANET));
+
+       Optional<Planet> sut = planetService.get(1L);
+
+       assertThat(sut).isNotEmpty();
+       assertThat(sut.get()).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void getPlanet_ByNonExistingId_ReturnsEmpty() {
+        when(planetRepository.findById(1L)).thenReturn(java.util.Optional.empty());
+
+        Optional<Planet> sut = planetService.get(1L);
+
+        assertThat(sut).isEmpty();
     }
 
 }
