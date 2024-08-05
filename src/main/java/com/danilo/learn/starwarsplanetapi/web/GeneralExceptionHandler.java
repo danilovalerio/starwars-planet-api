a1d9051d5daf4295ceb2fd358dd402b3baec00ac
danilovalerio <danilo_vs@hotmail.com>
2024-08-05 17:15:21 -0300

    Teste Sucesso - Tratamento da Exception DataIntegrityViolationException para retornar um conflict

--- src/main/java/com/danilo/learn/starwarsplanetapi/web/GeneralExceptionHandler.java
+++ src/main/java/com/danilo/learn/starwarsplanetapi/web/GeneralExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.danilo.learn.starwarsplanetapi.web;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -16,5 +18,16 @@ public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         return super.handleMethodArgumentNotValid(ex, headers, HttpStatus.UNPROCESSABLE_ENTITY, request);
     }
+
+    /**
+     * Quando ocorrer essa exceção ela será tratada e lançada um Status Conflict
+     * @param ex Exceção que será tratada
+     * @return Status de Conflict
+     */
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    private ResponseEntity<Object> handleConflict(DataIntegrityViolationException ex){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
 
 }
