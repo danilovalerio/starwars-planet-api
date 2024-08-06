28ea9dd95a68ca5ae7afeac1ec8664b950361a3f
danilovalerio <danilo_vs@hotmail.com>
2024-08-06 15:59:27 -0300

    Construtor com Id para testes

--- src/test/java/com/danilo/learn/starwarsplanetapi/common/PlanetConstants.java
+++ src/test/java/com/danilo/learn/starwarsplanetapi/common/PlanetConstants.java
@@ -10,9 +10,9 @@ public class PlanetConstants {
     public static final Planet PLANET = new Planet("name", "climate", "terrain");
     public static final Planet INVALID_PLANET = new Planet("", "", "");
 
-    public static final Planet TATOOINE = new Planet("Tatooine", "arid", "desert");
-    public static final Planet Alderaan = new Planet("Alderaan", "temperate", "grasslands, mountains");
-    public static final Planet YAVINIV = new Planet("Yavin IV", "temperate, tropical", "jungle, rainfores");
+    public static final Planet TATOOINE = new Planet(1L, "Tatooine", "arid", "desert");
+    public static final Planet Alderaan = new Planet(2L,"Alderaan", "temperate", "grasslands, mountains");
+    public static final Planet YAVINIV = new Planet(3L, "Yavin IV", "temperate, tropical", "jungle, rainfores");
 
     public static final List<Planet> PLANETS = Arrays.asList(TATOOINE, Alderaan, YAVINIV);
 
