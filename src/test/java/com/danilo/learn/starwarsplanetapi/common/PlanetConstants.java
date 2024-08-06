2eb57e80448e4e9e06d92274e6d1cc50ad11313d
danilovalerio <danilo_vs@hotmail.com>
2024-08-06 15:45:04 -0300

    Lista de planetas mock e script de planetas para o banco

--- src/test/java/com/danilo/learn/starwarsplanetapi/common/PlanetConstants.java
+++ src/test/java/com/danilo/learn/starwarsplanetapi/common/PlanetConstants.java
@@ -2,9 +2,18 @@ package com.danilo.learn.starwarsplanetapi.common;
 
 import com.danilo.learn.starwarsplanetapi.domain.Planet;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PlanetConstants {
 
     public static final Planet PLANET = new Planet("name", "climate", "terrain");
     public static final Planet INVALID_PLANET = new Planet("", "", "");
+
+    public static final Planet TATOOINE = new Planet("Tatooine", "arid", "desert");
+    public static final Planet Alderaan = new Planet("Alderaan", "temperate", "grasslands, mountains");
+    public static final Planet YAVINIV = new Planet("Yavin IV", "temperate, tropical", "jungle, rainfores");
+
+    public static final List<Planet> PLANETS = Arrays.asList(TATOOINE, Alderaan, YAVINIV);
 
 }
