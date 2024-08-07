28825d5333cf80fa3cdbdbfa3993b488055ae712
danilovalerio <danilo_vs@hotmail.com>
2024-08-07 10:18:57 -0300

    Teste Integrado Sucesso - Teste passando toda vez, e limpa tabela após cada método de teste utilizado

--- src/test/java/com/danilo/learn/starwarsplanetapi/PlanetIT.java
+++ src/test/java/com/danilo/learn/starwarsplanetapi/PlanetIT.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 import static com.danilo.learn.starwarsplanetapi.common.PlanetConstants.PLANET;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -18,6 +19,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
  */
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = {"/remove_planets.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class PlanetIT {
     @Autowired
     private TestRestTemplate restTemplate;
