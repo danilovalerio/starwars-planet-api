e49e389cbd0e63d6be8e029c0244cc1d2435972d
danilovalerio <danilo_vs@hotmail.com>
2024-08-07 10:11:39 -0300

    Teste Integrado Sucesso - Teste passando a primeira vez, a segunda dá erro pq já existe planeta na base

--- src/test/java/com/danilo/learn/starwarsplanetapi/PlanetIT.java
+++ src/test/java/com/danilo/learn/starwarsplanetapi/PlanetIT.java
@@ -1,8 +1,16 @@
 package com.danilo.learn.starwarsplanetapi;
 
+import com.danilo.learn.starwarsplanetapi.domain.Planet;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+
+import static com.danilo.learn.starwarsplanetapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 /**
  * Testes Integrados end2end e de componentes (subcutaneos)
@@ -11,9 +19,23 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PlanetIT {
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     //Testa o contexto da aplicação e valida as configurações dos beans
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void createPlanet_ReturnsCreated() {
+        //ForEntity - traduz o JSON para uma Entidade
+        ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(sut.getBody().getId()).isNotNull();
+        assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
+        assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
     }
 }
